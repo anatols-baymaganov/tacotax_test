a17a05cb569a1fc34ed6d328dd3a13b0c8a35599
Anatols Baymaganov <anatols.baymaganov@gmail.com>
2019-12-25 18:05:53 +0300

    Update Questionnaires::Parser logic
    * Return main elements of YAML file elements tree instead of
      elements array by method call
    * Move part of logic to Questionnaires::Element class
    * Update lines in Questionnaires::Creator related with yaml
      parsing

--- app/services/questionnaires/element.rb
+++ app/services/questionnaires/element.rb
@@ -2,6 +2,8 @@
 
 module Questionnaires
   class Element
+    ATTRIBUTES = %i[reference type label value content parent].freeze
+
     def initialize(reference, type, label, value, content)
       @reference = reference
       @type = type
@@ -13,17 +15,27 @@ module Questionnaires
       content.value&.each { |element| element.parent = self } if content.present?
     end
 
-    attr_reader :reference, :type, :label, :value, :content, :parent
+    attr_reader(*ATTRIBUTES)
 
     def [](key)
+      return unless key.to_sym.in?(ATTRIBUTES)
+
       public_send(key).value
     end
 
     def as_json
-      @as_json ||= begin
-        content&.value&.map!(&:as_json)
-        [reference, type, label, value, content].compact.map(&:as_json).inject({}, :merge)
-      end
+      return @as_json if defined?(@as_json)
+
+      @as_json = [reference, type, label, value].compact.map(&:as_json).inject({}, :merge)
+                                                .merge("content" => content&.value&.map(&:as_json))
+                                                .compact
+    end
+
+    def elements
+      return @elements if defined?(@elements)
+
+      @elements = content&.value.to_a.each_with_object([]) { |content, arr| arr << content.elements }.flatten << self
+      @elements.sort! { |a, b| a.line <=> b.line }
     end
 
     def line
